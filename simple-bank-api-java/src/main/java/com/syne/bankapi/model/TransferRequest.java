771dbad909e936edff18e576657e806e6923c29d
rakesoni <cserakesh.soni@gmail.com>
2026-05-10 23:01:41 +0530

    Add TransferRequest model for bank transfer

---
+++ simple-bank-api-java/src/main/java/com/syne/bankapi/model/TransferRequest.java
@@ -0,0 +1,32 @@
+package com.syne.bankapi.model;
+
+public class TransferRequest {
+
+    private int fromAccount;
+    private int toAccount;
+    private double amount;
+
+    public int getFromAccount() {
+        return fromAccount;
+    }
+
+    public void setFromAccount(int fromAccount) {
+        this.fromAccount = fromAccount;
+    }
+
+    public int getToAccount() {
+        return toAccount;
+    }
+
+    public void setToAccount(int toAccount) {
+        this.toAccount = toAccount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
